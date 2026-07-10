73ce82d25c35ed6306d9b033ae01454cb9fe6dbf
kWAY <55437151+kWAYTV@users.noreply.github.com>
2026-07-11 01:41:53 +0200

    fix(release): include tauri version files in bumpp commit

--- bump.config.mjs
+++ bump.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "bumpp";
 import tauri from "tauri-version";
 
 export default defineConfig({
+  all: true,
   files: ["package.json", "package-lock.json"],
   commit: "chore(release): bump version to %s",
   tag: "v",
